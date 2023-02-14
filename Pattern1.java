68d5c03a204eadef5b7d3a7f94176417edcfd351
Rishita77 <rishita.m8@gmail.com>
2023-02-14 22:56:40 +0530

    patterns

---
+++ Pattern1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pattern1 {
+    import java.util.Scanner;
+
+    public class Patterns {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+//        for (int i = 1; i <= n; i++) {
+//            System.out.print(i);
+//            for (int j = 1; j <= n; j++) {
+//                System.out.print(i);
+//
+//            }
+//            System.out.println();
+//        }
+//    }
+}
---
+++ Pattern3.java
@@ -0,0 +1,13 @@
+
+    import java.util.Scanner;
+
+    public class Pattern2 {
+for (int i = 1; i <= n ; i++) {
+//            for (int j = 1; j <=n ; j++) {
+//                System.out.print(n-j+1);
+//
+//            }
+//            System.out.println();
+//
+//        }
+}
